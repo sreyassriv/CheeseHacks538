cf273905b2ae479b02a47ce1a5eedced2593184f
Sreyas Srivastava <93051740+sreyassriv@users.noreply.github.com>
2022-11-12 15:42:11 -0600

    header files

---
+++ Title.js
@@ -0,0 +1,14 @@
+// src/Title.js
+
+import React from 'react'
+import './Title.css';
+
+function Title() {
+  return (
+    <div className="Title">
+      <h1>Reverse CookBook</h1><style>align = "center"</style>
+    </div>
+  )
+}
+
+export default Title;
