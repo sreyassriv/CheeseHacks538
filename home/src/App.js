2605c7e3a566d89e5709a0da5b1eb16caf886c1a
sreyassriv <sreyasssrivastava@gmail.com>
2022-11-12 16:08:49 -0600

    Full React Setup with files

---
+++ home/src/App.js
@@ -0,0 +1,15 @@
+// src/App.js
+
+import React from 'react';
+import './App.css';
+import Title from './Title';
+
+function App() {
+  return (
+    <div className="App">
+      <Title />
+    </div>
+  );
+}
+
+export default App;
